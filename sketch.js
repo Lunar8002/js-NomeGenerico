041adc1d978b399da7180d6b727f37343b73d9cc
Lunar_8002 <144026039+Lunar8002@users.noreply.github.com>
2023-09-11 09:28:39 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+
+    createCanvas(900, 900);
+    
+    background("black");
+    
+    }
+    
+    function draw() {
+    
+    stroke("blue");
+    
+    fill("red");
+    
+    if(mouseIsPressed){
+    
+    rect(mouseX, mouseY, 20, 35);
+    
+    }
+    
+    }    
